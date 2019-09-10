841b77685d856b8a7a30c04102552d97c99b79cc
guixiangyu <guixiangyu1@gmail.com>
2019-09-10 17:42:24 +0800

    init

--- model/data_utils.py
+++ model/data_utils.py
@@ -534,20 +534,18 @@ def get_chunks_from_tags(tags):
         # End of a chunk + start of a chunk!
         elif tok != 'O':
             tok_chunk_class, tok_chunk_type = tok.split('-')
-            if chunk_type is None:
+            if tok_chunk_class == "B":
+                chunk = (chunk_type, chunk_start, i)
+                chunks.append(chunk)
                 chunk_type, chunk_start = tok_chunk_type, i
-            else:
-                if tok_chunk_class == "B":
+            elif tok_chunk_class == 'I':
+                if tok_chunk_type == chunk_type:
+                    pass
+                else:
                     chunk = (chunk_type, chunk_start, i)
                     chunks.append(chunk)
-                    chunk_type, chunk_start = tok_chunk_type, i
-                elif tok_chunk_class == 'I':
-                    if tok_chunk_type == chunk_type:
-                        pass
-                    else:
-                        chunk = (chunk_type, chunk_start, i)
-                        chunks.append(chunk)
-                        chunk_type, chunk_start = None, None
+                    chunk_type, chunk_start = None, None
+
 
     # end condition
     if chunk_type is not None:
